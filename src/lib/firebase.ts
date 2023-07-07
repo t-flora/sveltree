a7468434c828b377f2f27d376e39b7f06c37e16e
Tiago Flora <tiagomflora@gmail.com>
2023-07-06 23:07:58 -0700

    add userStore for authentication

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import type { User } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from 'svelte/store';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCEk-atmGRQhiiC-fGXQ6gKon4mSKJfoNY",
@@ -17,3 +19,29 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+function userStore() {
+    // have an authentication state stored in a Svelte store
+    let unsubscribe: () => void;
+
+    if (!auth || !globalThis.window) {
+        console.warn("not authenticated or not in browser window");
+        const { subscribe } = writable<User|null>(null);
+        return {
+            subscribe,
+        }
+    }
+
+    const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+        const subscribe = onAuthStateChanged(auth, (user) => {
+            set(user);
+        })
+        return () => unsubscribe();
+    })
+
+    return {
+        subscribe,
+    }
+}
+
+export const user = userStore();
