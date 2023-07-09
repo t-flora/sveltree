668197add507143fa6d309cce5fc43a781b85899
Tiago Flora <tiagomflora@gmail.com>
2023-07-08 22:32:27 -0700

    add upload logic for profile images

--- src/routes/login/photo/+page.svelte
+++ src/routes/login/photo/+page.svelte
@@ -1,4 +1,31 @@
 <script lang="ts">
+    import { user, userData, storage, db } from '$lib/firebase';
+    import { doc, updateDoc } from 'firebase/firestore';
+    import { getDownloadURL, uploadBytes, ref } from 'firebase/storage';
+    import AuthCheck from '$lib/components/AuthCheck.svelte';
+
+    // define preview URL
+    let uploading = false;
+    let previewURL: string;
+    $: href = `/${$userData?.username}/edit`
+
+    /**
+     * Description
+     * @param {any} e:any
+     * @returns {undefined}
+     */
+    async function upload(e: any) {
+        uploading = true;
+        // select file and create URL for it
+        const file = e.target.files[0];
+        previewURL = URL.createObjectURL(file);
+        const storageRef = ref(storage, `users/${$user!.uid}/profile.png`) // why profile.png?
+        const result = await uploadBytes(storageRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc(db, "users", $user!.uid), {photoURL: url});
+        uploading = false;
+    }
+
 </script>
 
 <h2>photo</h2>
